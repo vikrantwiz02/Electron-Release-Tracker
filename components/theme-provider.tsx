3dbc82cbd9bb51fea949bafa9aa984427999b53c
vikrantwiz02 <vikrantkrd@gmail.com>
2025-03-26 00:00:47 +0530

    Refactor ThemeProvider component to define props type directly and improve import statements

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,19 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import type * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+  forcedTheme?: string
+  storageKey?: string
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
